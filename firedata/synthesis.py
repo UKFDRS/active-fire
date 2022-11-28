571fad168a0a96292ca2e360841dab283d670e6c
tadasnik <tadas.nik@gmail.com>
2022-11-28 15:02:11 +0000

    nlargest by continent

--- firedata/synthesis.py
+++ firedata/synthesis.py
@@ -6,6 +6,15 @@ continents_dict = {}
 sensor = "VIIRS_NPP"
 pc = populate_db.ProcSQL(sensor)
 # ad = pc.active_detections()
-sql_str = "SELECT * FROM events WHERE active = 1 order by tot_size desc limit 100"
+# sql_str = "SELECT * FROM events WHERE active = 1  GROUP BY continent order by tot_size desc limit 10"
+sql_str = """
+SELECT * FROM (
+    SELECT *,
+           row_number() over (
+               partition by continent order by tot_size desc
+               ) as continent_count
+    from events WHERE active = 1) ranks
+where continent_count <= 5;
+"""
 dfr = pc.db.return_many_values(sql_str)
 # dfr = pc.active_detections()
